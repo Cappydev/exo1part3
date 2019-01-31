403cde9af23c345e1730092913a142045bb7bff2
cappydev <kevin.lanel@wanadoo.fr>
2019-01-31 13:33:12 +0100

    corrigé exo1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//je declare ma fonction. Elle permet d'afficher la bordure rouge autour de l'image
+
+function borderRED() {
+  document.getElementById('image1').style.border = "5px solid red";
+}
+// je déclare ma fonction. Elle permet de retirer la bordure rouge autour de l'image
+function borderNone() {
+  document.getElementById('image1').style.border = '';
+}
